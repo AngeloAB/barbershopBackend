e5430f0972be6c33530c620cd126ba2dba73a2fa
AngeloAB <angelbvasquez0220@gmail.comcommit>
2025-05-20 09:16:14 -0400

    produccion ajuste nuevo con lok3

---
+++ routes/planes.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cron = require('node-cron');
+const Plan = require('../models/Planes');
+const Planes = require('../models/Planes');
+const router = express.Router();
+
+
+
+ 
+// Crear una venta
+router.post('/addPlan', async (req, res) => {
+  const {  name, price, funciones} = req.body;
+
+  try {
+    Plan.create({ name, price, funciones});
+    res.send({status: "Exito", data: "Plan creado"});
+  } catch (error) {
+    return res.send({error: error});
+  }
+});
+
+
+router.post('/plandata', async (req, res) => {
+  try{
+
+      Plan.find().then((data) =>{
+          return res.send({status: "Exito", data: data});
+      });
+  } catch(error){
+     return res.send({error: error});
+  }
+});
+
+// Actualizar un servicio
+router.post('/update/:id', async (req, res) => {
+  const {planId, name, price, funciones } = req.body;
+
+    try{
+      
+        await Plan.updateOne(
+            {_id: planId},
+            {
+                $set:{
+                    name,
+                    price,
+                    funciones
+
+                },
+            }
+        );
+        res.send({status: "Exito", data: "Plan Actualizado"});
+
+    }catch(error){
+        return res.send({error: error});
+    }
+});
+
+// Eliminar un servicio
+router.post('/delete/:id', async (req, res) => {
+  const {planId} = req.body;
+
+    try {
+      
+      await Plan.deleteOne({_id: planId});
+      res.send({status: "Exito", data: "Plan Eliminado"});
+
+    }catch(error){
+        return res.send({error: error});
+    }
+});
+
+module.exports = router;
