cbcd5baea9b4eaf116eb59d0872898823c9e7dd5
AngeloAB <angelbvasquez0220@gmail.comcommit>
2025-07-01 16:09:12 -0400

    produccion ajuste nuevo con lok23

--- models/Planes.js
+++ models/Planes.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PlanesSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: String, required: true },
-  funciones: { type: String, required: true },
+  funciones: { type: [String], required: true },
 });
 
 module.exports = mongoose.model('Planes', PlanesSchema);
